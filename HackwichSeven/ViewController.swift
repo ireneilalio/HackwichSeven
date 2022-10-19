6c160fab7732578991b68482cda4089b96d9b267
Irene Ilalio <irenei@hawaii.edu>
2022-10-18 23:57:21 -1000

    Completed parts 1-8

--- HackwichSeven/ViewController.swift
+++ HackwichSeven/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Part5: Create IBOutlets for Text Field and Label
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //Part6: Set initial label text to no text in viewDidLoad
+        self.displayLabel.text = ""
+
     }
+    //Part7: Create and IB action for the button
+    @IBAction func setLabelButtonPressed(_ sender: Any)
+    {
+        //Part8: Set up IB action to display user input text
+        let userInputText = textField.text
+        
+        //set the display label to display the text the user inputs
+        self.displayLabel.text = userInputText
+        
 
-
+    }
+    
 }
 
